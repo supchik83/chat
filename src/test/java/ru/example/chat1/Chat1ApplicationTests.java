9236f59eadc5b21efb187d8c3ca6fa22a14807d4
supchik83 <melens@bk.ru>
2021-05-22 15:10:37 +0300

    test

---
+++ src/test/java/ru/example/chat1/Chat1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.example.chat1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chat1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
